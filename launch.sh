e3296e5fa786584278cbdd1a42952c94237db43e
BobbyZhouZijian <zephyroszhou@gmail.com>
2020-05-24 22:40:51 +0800

    update bash

--- launch.sh
+++ launch.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
-read -p "Ensure you have install python3, jupyter notebook, pandas, numpy, ta and scipy [y/n]: " chk
-
-chk=$( echo $chk | tr '[:upper:]' '[:lower:]' )
- 
-if [ $chk != y ] && [ $chk != yes ]   
-then
-    echo please install the dependencies first. quitting ...
-    exit 1
-fi
-
-read -p "enter the currency pair in the format (EURUSD): " currency
-
-for name in `find ./Dataframes -type f -exec basename {} \;`
-    do
-        basename=${name%%.*}
-        if [[ df_$currency = $basename ]]
-            then
-            read -p "existing forex pair df detected, skip preparation?[y/n]:" skip
-        fi
-done
-
-skip=$( echo $skip | tr '[:upper:]' '[:lower:]' )
-
 
 prep() {
     echo determining python3 path
@@ -42,22 +19,52 @@ prep() {
     fi
 }
 
+toLowerCase() {
+   echo $( echo $1 | tr '[:upper:]' '[:lower:]' )
+}
+
+read -p "Ensure you have install python3, jupyter notebook, pandas, numpy, ta and scipy [y/n]: " chk
+
+chk=$(toLowerCase $chk)
+
+if [ $chk != y ] && [ $chk != yes ]   
+then
+    echo please install the dependencies first. quitting ...
+    exit 1
+fi
+
+read -p "enter the currency pair in the format (eg. EURUSD): " currency
+
+for name in `find ./Dataframes -type f -exec basename {} \;`
+    do
+        basename=${name%%.*}
+        if [[ df_$currency = $basename ]]
+            then
+            read -p "existing forex pair df detected, skip preparation?[y/n]: " skip
+        fi
+done
+
+skip=$(toLowerCase $skip)
+
+
 if [[ $skip != "yes" && $skip != "y" ]]
 then  prep
 fi
 
-    read -p "which model do you want to refer to? [LSTM/Classifiers]: " ans
-    ans=$(echo $ans | tr '[:upper:]' '[:lower:]')
-    if [ $ans = 'lstm' ]
-    then
-        echo LSTM chosen, starting jupyter notebook
-        jupyter notebook --no-browser --port 8888 ./Neural_Networks_Model/LSTM_Template.ipynb
-    else
-        echo Classifiers chosen, starting jupyter notebook
-        jupyter notebook --no-browser --port 8888 ./Classifiers_Model/Classifiers_Template.ipynb
-    fi
+read -p "which model do you want to refer to? [LSTM/Classifiers]: " ans
+ans=$(toLowerCase $ans)
+if [ $ans = 'lstm' ]
+then
+    echo LSTM chosen, starting jupyter notebook
+    cd ./Neural_Networks_Model
+    jupyter notebook --no-browser --port 8888 
+else
+    echo Classifiers chosen, starting jupyter notebook
+    cd ./Classifiers_Model
+    jupyter notebook --no-browser --port 8888 
+fi
 
-    if [ $? -eq 0 ]
-       then echo successfully launed jupyter notebook\nnavigate to \
-            localhost:8888 in browser to have a look 
-    fi
+if [ $? -eq 0 ]
+    then echo successfully launed jupyter notebook\nnavigate to \
+        localhost:8888 in browser to have a look 
+fi
